3c87edccbc108b19f840db5acd7aa5379264bbc5
Lukas Mösle <ext.dl.moesle@muenchen.de>
2022-04-27 17:42:07 +0200

    fix download url

--- src/util/FileUtils.ts
+++ src/util/FileUtils.ts
@@ -1,6 +1,6 @@
 import { ArtifactMilestoneTO } from "../api";
 
-const BASE_PATH = process.env.REACT_APP_BACKEND ?? "/";
+const BASE_PATH = process.env.REACT_APP_BACKEND ?? "";
 
 export const downloadFile = (milestone: ArtifactMilestoneTO): void => {
     const filePath = `${BASE_PATH}/api/milestone/${milestone.artifactId}/${milestone.id}/download`;
